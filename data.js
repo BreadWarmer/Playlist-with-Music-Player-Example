7395cdc506590871bddaec8baba04fb3227c16ce
BreadWarmer <78377006+BreadWarmer@users.noreply.github.com>
2023-07-22 22:18:49 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Sleepyhead',
+        path: 'assets/musics/Sleepyhead.mp3',
+        artist: 'Passion Pit',
+        cover: 'assets/images/sleepyhead.jpg'
+    },
+    {
+        name: 'Love Is Awake',
+        path: 'assets/musics/LoveIsAwake.mp3',
+        artist: 'Madnap, Late Night Alumni',
+        cover: 'assets/images/loveisawake.jpg'
+    },
+    {
+        name: 'Burnin',
+        path: 'assets/musics/Burnin.mp3',
+        artist: 'Petit Biscuit',
+        cover: 'assets/images/burnin.jpg'
+    },
+    {
+        name: 'Across The Room',
+        path: 'assets/musics/AcrossTheRoom.mp3',
+        artist: 'ODESZA, Leon Bridges',
+        cover: 'assets/images/acrosstheroom.jpg'
+    },
+    {
+        name: 'Your Tempo',
+        path: 'assets/musics/YourTempo.mp3',
+        artist: 'Kasbo',
+        cover: 'assets/images/yourtempo.jpg'
+    },
+    {
+        name: 'Shelter',
+        path: 'assets/musics/Shelter.mp3',
+        artist: 'Porter Robinson, Madeon',
+        cover: 'assets/images/shelter.jpg'
+    },
+    {
+        name: 'On The Road',
+        path: 'assets/musics/OnTheRoad.mp3',
+        artist: 'Petit Biscuit',
+        cover: 'assets/images/ontheroad.jpg'
+    },
+    {
+        name: 'Young',
+        path: 'assets/musics/Young.mp3',
+        artist: 'Vallis Alps',
+        cover: 'assets/images/young.jpg'
+    },
+]
